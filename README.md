2d56543986b193a26592078a661f59851196801f
Alexander Razzhivin <admin@httpbots.com>
2016-10-17 00:11:52 +0300

    added atoprefixer and sourcemaps

---
+++ README.md
@@ -0,0 +1,17 @@
+## Simple base project structure
+
+### Libs
+
+https://github.com/sass/node-sass
+https://github.com/postcss/autoprefixer
+
+### Setup
+
+npm install
+
+### Reference articles
+
+Gulp, Sass, Autoprefixer, Sourcemaps!
+https://github.com/ai/browserslist
+https://github.com/sass/node-sass#options
+https://fettblog.eu/blog/2014/04/10/gulp-sass-autoprefixer-sourcemaps/
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var prefix = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 
 var appDir = 'app';
 
 gulp.task('sass', function() {                  // Создаем таск Sass
   return gulp.src(appDir+'/scss/**/*.+(scss|sass)')     // Берем источник
+      .pipe(sourcemaps.init())                  // Инициализируем sourcemap для отладки
       .pipe(sass({
-          outputStyle: 'compressed',
+          outputStyle: 'nested',                // Возможные значения: nested, expanded, compact, compressed
           includePaths: ['node_modules/susy/sass']
-      }).on('error', sass.logError))            // Преобразуем Sass в CSS посредством gulp-sass
+      })
+      .on('error', sass.logError))            // Преобразуем Sass в CSS посредством gulp-sass
+      .pipe(prefix("> 5%", 'ie >= 9'))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest( appDir+'/css') )         // Выгружаем результата в папку app/css
       .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
